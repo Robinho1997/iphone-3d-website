2df6c9e4f86deae7d70edc1697d2e1d758a2c2cc
Robinho1997 <robin.kempen@gmx.de>
2023-05-13 21:51:34 +0200

    handle Preview

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 import Nav from "./components/Nav";
 import Jumbatron from "./components/Jumbotron";
 import SoundSection from "./components/SoundSection";
+import DisplaySection from "./components/DisplaySection";
+import WebgiViewer from "./components/WebgiViewer";
 
 function App() {
+  const webgiViewerRef = useRef();
+  const contentRef = useRef();
+
+  const handlePreview = () => {
+    webgiViewerRef.current.triggerPreview();
+  }
+
 
   return (
     <div className="App">
-      <Nav/>
-      <Jumbatron/>
-      <SoundSection/>
+      <div ref={contentRef} id="content">
+        <Nav />
+        <Jumbatron />
+        <SoundSection />
+        <DisplaySection triggerPreview={handlePreview} />
+      </div>
+      <WebgiViewer contentRef={contentRef} ref={webgiViewerRef} />
     </div>
   );
 }
